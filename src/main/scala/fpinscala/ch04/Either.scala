6ce743d4e7edde960e09d05fd3c8d93eadbe9493
Jan Ferko <julyloov@gmail.com>
2019-01-20 21:52:00 +0100

    Add description for exercise 4.8

--- src/main/scala/fpinscala/ch04/Either.scala
+++ src/main/scala/fpinscala/ch04/Either.scala
@@ -29,6 +29,7 @@ sealed trait Either[+E, +A] {
 }
 
 object Either {
+  // Exercise 4.7
   def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
     es.foldRight(Right(Nil): Either[E, List[A]]) { (e, acc) =>
       e.flatMap(a => acc.map(as => a :: as))
@@ -38,6 +39,14 @@ object Either {
     as.foldRight(Right(Nil): Either[E, List[B]]) { (a, acc) =>
       f(a).flatMap(b => acc.map(bs => b :: bs))
     }
+
+  // Exercise 4.8
+  //
+  // We can introduce new data type called Validation
+  // that captures multiple error into list Either[List[E], A]
+  // and also has map2 function and either can be transform into it.
+  // So in mkPerson we run validations that return either, than convert them
+  // into validation datatype and use map2 over it.
 }
 case class Left[E](value: E) extends Either[E, Nothing]
 case class Right[A](value: A) extends Either[Nothing, A]
